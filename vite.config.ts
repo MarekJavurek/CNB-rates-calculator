f974406809724093e8847958be562aa29dc415fb
Marek JAVŮREK <peanee@gmail.com>
2025-11-25 21:35:51 +0100

    fix loading @mui/styled-engine-sc

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    },
+  },
   envPrefix: "FE_",
 });
